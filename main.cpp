2e5873f4d8cf85c58a38324e3de7d9088630d9b4
tp19121909 <mk.19121909@gmail.com>
2022-01-11 21:10:55 +0100

    02 Zmiana Policz

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 
 int Policz(int a, int b)
 {
-    return a + b;
+    return a * b;
 }
 
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "W pierwszym branchu; 2+3=" << Policz(2,3) << std::endl;
+    std::cout << "W pierwszym branchu; 2*3=" << Policz(2,3) << std::endl;
     return 0;
 }
